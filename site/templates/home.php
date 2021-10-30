e49be043fda5a2d87a46f0abc71d30eef0a4cf32
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2021-10-30 13:56:21 +0200

    added template for example

---
+++ site/templates/home.php
@@ -0,0 +1,89 @@
+<?php snippet('header') ?>
+
+<div class="banner">
+    <div class="banner__background">
+        background-img
+
+        <div class="banner__background__animation">
+            animation
+        </div>
+
+        <div class="banner__background__scroll">
+            scroll
+        </div>
+    </div>
+</div>
+
+
+
+<section class="message">
+    <h2 class="message__title">Title</h2>
+    <p class="message__paragraph">Paragraph</p>
+</section>
+
+
+
+<section class="introduction">
+    <h2 class="introduction__title">Title</h2>
+
+    <div class="introduction__flexbox">
+        <div class="introduction__flexbox__left">
+            <p class="introduction__flexbox__left__paragraph">Paragraph</p>
+            <a class="introduction__flexbox__left__button" href="#">Button</a>
+        </div>
+
+        <div class="introduction__flexbox__right">
+            <div class="introduction__flexbox__right__openhours">
+                <h3 class="introduction__flexbox__right__openhours__title">Title</h3>
+                <p class="introduction__flexbox__right__openhours__p">Uren</p>
+            </div>
+
+            <div class="introduction__flexbox__right__openhours">
+                <h3 class="introduction__flexbox__right__openhours__title">Title</h3>
+                <p class="introduction__flexbox__right__openhours__p">Uren</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section class="working">
+    <h2 class="working__title">Title</h2>
+
+    <div class="working__flexbox">
+        <div class="working__flexbox__left">
+            <p class="working__flexbox__left__paragraph">Paragraph</p>
+
+            <h3 class="working__flexbox__left__title"></h3>
+            <p class="working__flexbox__left__paragraph">Paragraph</p>
+        </div>
+
+        <div class="working__flexbox__right">
+            img
+        </div>
+    </div>
+</section>
+
+
+
+<section class="biolab">
+    <h2 class="biolab__title">Title</h2>
+
+    <div class="biolab__flexbox">
+        <div class="biolab__flexbox__left">
+            <p class="biolab__flexbox__left__paragraph">Paragraph</p>
+            <a class="biolab__flexbox__left__button" href="#"></a>
+        </div>
+
+        <div class="working__flexbox__right">
+            img
+        </div>
+    </div>
+</section>
+
+
+
+<?php snippet('partners') ?>
+
+<?php snippet('footer') ?>
