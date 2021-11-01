6af08b8670940242b708a7978f9870528c176685
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2021-11-01 09:58:56 +0100

    temp machine setup

--- site/templates/machine.php
+++ site/templates/machine.php
@@ -14,18 +14,63 @@
 
                     <dl>
                         <?php if ($page->category()->isNotEmpty()): ?>
-                            <dt>Client</dt>
-                            <dd><?= $page->client() ?></dd>
-                        <?php endif ?>
-
-                        <?php if ($page->category()->isNotEmpty()): ?>
                             <dt>Category</dt>
                             <dd><?= $page->category() ?></dd>
                         <?php endif ?>
 
-                        <?php if ($page->link()->isNotEmpty()): ?>
-                            <dt>Link</dt>
-                            <dd><?= $page->link() ?></dd>
+                        <?php if ($page->manufactur()->isNotEmpty()): ?>
+                            <dt>Fabrikant</dt>
+                            <dd><?= $page->manufactur() ?></dd>
+                        <?php endif ?>
+
+                        <?php if ($page->buildVolume()->isNotEmpty()): ?>
+                            <dt>Build Volume</dt>
+                            <dd><?= $page->buildVolume() ?></dd>
+                        <?php endif ?>
+
+                        <?php if ($page->nozzle()->isNotEmpty()): ?>
+                            <dt>Nozzle</dt>
+                            <dd><?= $page->nozzle() ?></dd>
+                        <?php endif ?>
+
+                        <?php if ($page->filament()->isNotEmpty()): ?>
+                            <dt>Filament</dt>
+                            <dd><?= $page->filament() ?></dd>
+                        <?php endif ?>
+
+                        <?php if ($page->layerHeights()->isNotEmpty()): ?>
+                            <dt>Layer Heights</dt>
+                            <dd><?= $page->layerHeights() ?></dd>
+                        <?php endif ?>
+
+                        <?php if ($page->bed()->isNotEmpty()): ?>
+                            <dt>Bed</dt>
+                            <dd><?= $page->bed() ?></dd>
+                        <?php endif ?>
+
+                        <?php if ($page->supportedFileTypes()->isNotEmpty()): ?>
+                            <dt>Supported File Types</dt>
+                            <dd><?= $page->supportedFileTypes() ?></dd>
+                        <?php endif ?>
+
+                        <?php if ($page->software()->isNotEmpty()): ?>
+                            <dt>Software</dt>
+                            <dd><?= $page->software() ?></dd>
+                        <?php endif ?>
+
+                        <?php if ($page->manual()->isNotEmpty()): ?>
+                            <dt>Manual</dt>
+                            <dd><?= $page->manual() ?></dd>
+                        <?php endif ?>
+
+                        <?php if ($page->tutorial()->isNotEmpty()): ?>
+                            <dt>Tutorial</dt>
+                            <dd><?= $page->tutorial() ?></dd>
+                        <?php endif ?>
+
+                        <?php if ($page->creation()->isNotEmpty()): ?>
+                            <dt>Creation</dt>
+                            <dd><?= $page->creation() ?></dd>
                         <?php endif ?>
                     </dl>
                 </div>
