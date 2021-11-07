6e4bb62b2d5d33bc43ea9022ff4acf836071151b
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2021-11-07 10:07:34 +0100

    bizzy on css home

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -5,7 +5,7 @@
 
                 <!-- BLOCK -->
                 <div class="footer__content__block">
-                    <h4 class="footer__content__block__title footer-h4 first-title h4">Contact</h4>
+                    <h4 class="footer__content__block__title h4-footer first-title h4">Contact</h4>
                     <a class="footer__content__block__link" href="#"><i class="fa fa-map-marker location-icon" aria-hidden="true"></i> Adres</a>
                     <a class="footer__content__block__link footer__content__block__link-email" href="#"><i class="fa fa-envelope" aria-hidden="true"></i> Mail</a>
 
@@ -15,7 +15,7 @@
 
                 <!-- BLOCK - DESKTOP LAYOUT SITEMAP -->
                 <div id="sitemap-desktop" class="footer__content__block">
-                    <h4 class="footer__content__block__title footer-h4 h4">Navigatie</h4>
+                    <h4 class="footer__content__block__title h4-footer h4">Navigatie</h4>
                     <a class="footer__content__block__link" href="#">Home</a>
                     <a class="footer__content__block__link" href="#">Machines</a>
                     <a class="footer__content__block__link" href="#">Reserveren</a>
@@ -25,7 +25,7 @@
 
                 <!-- BLOCK - DESKTOP LAYOUT PAGES -->
                 <div id="pages-desktop" class="footer__content__block">
-                    <h4 class="footer__content__block__title footer-h4 h4">Info</h4>
+                    <h4 class="footer__content__block__title h4-footer h4">Info</h4>
                     <a class="footer__content__block__link" href="#">FAQ</a>
                     <a class="footer__content__block__link" href="#">Huisregels</a>
                     <a class="footer__content__block__link" href="./price-list">Prijslijst</a>
@@ -34,15 +34,15 @@
 
                 <!-- BLOCK -->
                 <div class="footer__content__block">
-                    <h4 class="footer__content__block__title footer-h4 h4">Openingsuren</h4>
+                    <h4 class="footer__content__block__title h4-footer h4">Openingsuren</h4>
 
                     <div class="footer__content__block__openhours">
-                        <h5 class="footer__content__block__openhours__title footer-h5 h5">KdG-medewerkers/studenten</h5>
+                        <h5 class="footer__content__block__openhours__title h5-footer h5">KdG-medewerkers/studenten</h5>
                         <p class="footer__content__block__openhours__paragraph">ma - do: 9.00u - 17.00u</p>
                     </div>
 
                     <div class="footer__content__block__openhours">
-                        <h5 class="footer__content__block__openhours__title footer-h5 h5">Open lab momenten</h5>
+                        <h5 class="footer__content__block__openhours__title h5-footer h5">Open lab momenten</h5>
                         <p class="footer__content__block__openhours__paragraph">woe - do: 13.00u - 17.00u</p>
                     </div>
                 </div>
@@ -50,7 +50,7 @@
                 <!-- BLOCK - MOBILE LAYOUT SITEMAP + PAGES -->
                 <div class="footer__content__flexbox">
                     <div class="footer__content__flexbox__block">
-                        <h4 class="footer__content__block__title footer-h4 h4">Navigatie</h4>
+                        <h4 class="footer__content__block__title h4-footer h4">Navigatie</h4>
                         <a class="footer__content__block__link" href="#">Home</a>
                         <a class="footer__content__block__link" href="#">Machines</a>
                         <a class="footer__content__block__link" href="#">Reserveren</a>
@@ -59,7 +59,7 @@
                     </div>
 
                     <div class="footer__content__flexbox__block">
-                        <h4 class="footer__content__block__title footer-h4 h4">Info</h4>
+                        <h4 class="footer__content__block__title h4-footer h4">Info</h4>
                         <a class="footer__content__block__link" href="#">FAQ</a>
                         <a class="footer__content__block__link" href="#">Huisregels</a>
                         <a class="footer__content__block__link" href="./price-list">Prijslijst</a>
@@ -69,7 +69,7 @@
 
                 <!-- BLOCK -->
                 <div class="footer__content__block">
-                    <h4 class="footer__content__block__title footer-h4 h4">Met steun van</h4>
+                    <h4 class="footer__content__block__title h4-footer h4">Met steun van</h4>
 
                     <div class="footer__content__block__imageholder">
                         <img class="footer__content__block__imageholder__img" src="assets/images/footer-kdg.webp" alt="KdG logo">
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,160 +1,172 @@
 <!-- SNIPPET - HEADER -->
 <?php snippet('header') ?>
+    <div class="home-container">
 
-    <!-- BANNER -->
-    <div class="banner">
-        <div class="banner__background">
-            background-img
+        <!-- BANNER -->
+        <div class="home-container__banner">
+            <div class="home-container__banner__background">
+                background-img
 
-            <div class="banner__background__animation">
-                animation
-            </div>
-
-            <div class="banner__background__scroll">
-                scroll
-            </div>
-        </div>
-    </div>
-
-
-
-    <!-- OPTIONAL MESSAGE FABLAB CLOSED -->
-    <section class="message">
-
-        <!-- messageTitle -->
-        <?php if($page->messageTitle()->isNotEmpty()): ?>
-            <h2 class="message__title"><?= $page->messageTitle() ?></h2>
-        <?php endif; ?>
-
-        <!-- messageText -->
-        <?php if($page->messageText()->isNotEmpty()): ?>
-            <p class="message__paragraph"><?= $page->messageText() ?></p>
-        <?php endif; ?>
-    </section>
-
-
-
-    <!-- INTRODUCTION FABLAB -->
-    <section class="introduction">
-
-        <!-- whatIsFablabTitle -->
-        <?php if($page->whatIsFablabTitle()->isNotEmpty()): ?>
-            <h2 class="introduction__title"><?= $page->whatIsFablabTitle() ?></h2>
-        <?php endif; ?>
-
-        <div class="introduction__flexbox">
-            <div class="introduction__flexbox__left">
-
-                <!-- whatIsFablabText -->
-                <?php if($page->whatIsFablabText()->isNotEmpty()): ?>
-                    <p class="introduction__flexbox__left__paragraph"><?= $page->whatIsFablabText() ?></p>
-                <?php endif; ?>
-
-                <!-- whatIsFablabButtonText -->
-                <?php if($page->whatIsFablabButtonText()->isNotEmpty()): ?>
-                    <a class="introduction__flexbox__left__button" href="#"><?= $page->whatIsFablabButtonText() ?> <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
-                <?php endif; ?>
-            </div>
-
-            <div class="introduction__flexbox__right">
-                <div class="introduction__flexbox__right__openhours">
-
-                    <!-- openhoursKdGTitle -->
-                    <?php if($page->openhoursKdGTitle()->isNotEmpty()): ?>
-                        <h3 class="introduction__flexbox__right__openhours__title"><?= $page->openhoursKdGTitle() ?></h3>
-                    <?php endif; ?>
-
-                    <!-- openhoursKdGText -->
-                    <?php if($page->openhoursKdGText()->isNotEmpty()): ?>
-                        <p class="introduction__flexbox__right__openhours__paragraph"><?= $page->openhoursKdGText() ?></p>
-                    <?php endif; ?>
+                <div class="home-container__banner__background__animation">
+                    animation
                 </div>
 
-                <div class="introduction__flexbox__right__openhours">
-
-                    <!-- openhoursExternTitle -->
-                    <?php if($page->openhoursExternTitle()->isNotEmpty()): ?>
-                        <h3 class="introduction__flexbox__right__openhours__title"><?= $page->openhoursExternTitle() ?></h3>
-                    <?php endif; ?>
-
-                    <!-- openhoursExternText -->
-                    <?php if($page->openhoursExternText()->isNotEmpty()): ?>
-                        <p class="introduction__flexbox__right__openhours__paragraph"><?= $page->openhoursExternText() ?></p>
-                    <?php endif; ?>
+                <div class="home-container__banner__background__scroll">
+                    scroll
                 </div>
             </div>
         </div>
-    </section>
 
 
 
-    <!-- HOW DOES FABLAB WORK -->
-    <section class="working">
+        <!-- OPTIONAL MESSAGE FABLAB CLOSED -->
+        <section class="home-container__message">
 
-        <!-- workingFablabTitle -->
-        <?php if($page->workingFablabTitle()->isNotEmpty()): ?>
-            <h2 class="working__title"><?= $page->workingFablabTitle() ?></h2>
-        <?php endif; ?>
-
-        <div class="working__flexbox">
-            <div class="working__flexbox__left">
-
-                <!-- workingFablabText -->
-                <?php if($page->workingFablabText()->isNotEmpty()): ?>
-                    <p class="working__flexbox__left__paragraph"><?= $page->workingFablabText() ?></p>
-                <?php endif; ?>
-
-                <!-- workingFablabButtonText -->
-                <?php if($page->workingFablabButtonText()->isNotEmpty()): ?>
-                    <a class="working__flexbox__left__button" href="#"><?= $page->workingFablabButtonText() ?> <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
-                <?php endif; ?>
-            </div>
-
-            <!-- workingFablabImage -->
-            <?php if($page->workingFablabImage()->isNotEmpty()): ?>
-                <div class="working__flexbox__right">
-                    <img src="<?= $page->workingFablabImage()->url() ?>" alt="Werking Fablab image">
-                </div>
+            <!-- messageTitle -->
+            <?php if($page->messageTitle()->isNotEmpty()): ?>
+                <h2 class="home-container__message__title h2"><?= $page->messageTitle() ?></h2>
             <?php endif; ?>
-        </div>
-    </section>
 
-    
+            <!-- messageText -->
+            <?php if($page->messageText()->isNotEmpty()): ?>
+                <p class="home-container__message__paragraph p"><?= $page->messageText() ?></p>
+            <?php endif; ?>
+        </section>
 
-    <!-- LINK BIOLAB -->
-    <section class="biolab">
 
-        <!-- biolabTitle -->
-        <?php if($page->biolabTitle()->isNotEmpty()): ?>
-            <h2 class="biolab__title"><?= $page->biolabTitle()->url() ?></h2>
-        <?php endif; ?>
 
-        <div class="biolab__flexbox">
-            <div class="biolab__flexbox__left">
+        <!-- INTRODUCTION FABLAB -->
+        <section class="home-container__introduction">
 
-                <!-- biolabText -->
-                <?php if($page->biolabText()->isNotEmpty()): ?>
-                    <p class="biolab__flexbox__left__paragraph"><?= $page->biolabText()?></p>
-                <?php endif; ?>
+            <!-- whatIsFablabTitle -->
+            <?php if($page->whatIsFablabTitle()->isNotEmpty()): ?>
+                <h2 class="home-container__introduction__title h2-home h2"><?= $page->whatIsFablabTitle() ?></h2>
+            <?php endif; ?>
 
-                <!-- biolabButtonText -->
-                <?php if($page->biolabButtonText()->isNotEmpty()): ?>
-                    <a class="biolab__flexbox__left__button" href="#"><?= $page->biolabButtonText()?> <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+            <div class="home-container__introduction__flexbox">
+                <div class="home-container__introduction__flexbox__left">
+
+                    <!-- whatIsFablabText -->
+                    <?php if($page->whatIsFablabText()->isNotEmpty()): ?>
+                        <p class="home-container__introduction__flexbox__left__paragraph p"><?= $page->whatIsFablabText() ?></p>
+                    <?php endif; ?>
+
+                    <!-- whatIsFablabButtonText -->
+                    <?php if($page->whatIsFablabButtonText()->isNotEmpty()): ?>
+                        <a class="home-container__introduction__flexbox__left__button button-small" href="#"><?= $page->whatIsFablabButtonText() ?> <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                    <?php endif; ?>
+                </div>
+
+                <div class="home-container__introduction__flexbox__right">
+                    <div class="home-container__introduction__flexbox__right__openhours">
+
+                        <!-- openhoursKdGTitle -->
+                        <?php if($page->openhoursKdGTitle()->isNotEmpty()): ?>
+                            <h3 class="home-container__introduction__flexbox__right__openhours__title h3"><?= $page->openhoursKdGTitle() ?></h3>
+                        <?php endif; ?>
+
+                        <!-- openhoursKdGText -->
+                        <?php if($page->openhoursKdGText()->isNotEmpty()): ?>
+                            <p class="home-container__introduction__flexbox__right__openhours__paragraph p"><?= $page->openhoursKdGText() ?></p>
+                        <?php endif; ?>
+                    </div>
+
+                    <div class="home-container__introduction__flexbox__right__openhours">
+
+                        <!-- openhoursExternTitle -->
+                        <?php if($page->openhoursExternTitle()->isNotEmpty()): ?>
+                            <h3 class="home-container__introduction__flexbox__right__openhours__title h3"><?= $page->openhoursExternTitle() ?></h3>
+                        <?php endif; ?>
+
+                        <!-- openhoursExternText -->
+                        <?php if($page->openhoursExternText()->isNotEmpty()): ?>
+                            <p class="home-container__introduction__flexbox__right__openhours__paragraph p"><?= $page->openhoursExternText() ?></p>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <!-- HOW DOES FABLAB WORK -->
+        <section class="home-container__working">
+
+            <!-- WORKING FABLAB BANNER IMAGE -->
+            <div class="home-container__working__banner">
+
+                <!-- workingFablabTitle -->
+                <?php if($page->workingFablabTitle()->isNotEmpty()): ?>
+                    <h2 class="home-container__working__banner__title h2-home h2"><?= $page->workingFablabTitle() ?></h2>
                 <?php endif; ?>
             </div>
 
-            <div class="working__flexbox__right">
+            <div class="home-container__working__flexbox">
+                <div class="home-container__working__flexbox__left">
 
-                <!-- biolabImage -->
-                <?php if($page->biolabImage()->isNotEmpty()): ?>
-                    <img src="<?= $page->biolabImage()->url() ?>" alt="Biolab image">
+                    <!-- workingFablabText -->
+                    <?php if($page->workingFablabText()->isNotEmpty()): ?>
+                        <p class="home-container__working__flexbox__left__paragraph p"><?= $page->workingFablabText() ?></p>
+                    <?php endif; ?>
+
+                    <!-- workingFablabButtonText -->
+                    <?php if($page->workingFablabButtonText()->isNotEmpty()): ?>
+                        <a class="home-container__working__flexbox__left__button button-small" href="#"><?= $page->workingFablabButtonText() ?> <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                    <?php endif; ?>
+                </div>
+
+                <!-- workingFablabImage DESKTOP-->
+                <?php if($page->workingFablabImage()->isNotEmpty()): ?>
+                    <div class="home-container__working__flexbox__right">
+                        <img src="<?= $page->workingFablabImage()->url() ?>" alt="Werking Fablab image">
+                    </div>
                 <?php endif; ?>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <!-- SNIPPET - PARTNERS -->
-    <?php snippet('partners') ?>
+        
+
+        <!-- LINK BIOLAB -->
+        <section class="home-container__biolab">
+
+            <!-- BIOLAB BANNER IMAGE -->
+            <div class="home-container__biolab__banner">
+
+                <!-- biolabTitle -->
+                <?php if($page->biolabTitle()->isNotEmpty()): ?>
+                    <h2 class="home-container__biolab__banner__title h2-home h2"><?= $page->biolabTitle()->url() ?></h2>
+                <?php endif; ?>
+            </div>
+
+            <div class="home-container__biolab__flexbox">
+                <div class="home-container__biolab__flexbox__left">
+
+                    <!-- biolabText -->
+                    <?php if($page->biolabText()->isNotEmpty()): ?>
+                        <p class="home-container__biolab__flexbox__left__paragraph"><?= $page->biolabText()?></p>
+                    <?php endif; ?>
+
+                    <!-- biolabButtonText -->
+                    <?php if($page->biolabButtonText()->isNotEmpty()): ?>
+                        <a class="home-container__biolab__flexbox__left__button button-small" href="#"><?= $page->biolabButtonText()?> <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                    <?php endif; ?>
+                </div>
+
+                <div class="home-container__biolab__flexbox__right">
+
+                    <!-- biolabImage DESKTOP -->
+                    <?php if($page->biolabImage()->isNotEmpty()): ?>
+                        <div class="home-container__biolab__flexbox__right">
+                            <img src="<?= $page->biolabImage()->url() ?>" alt="Biolab image">
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </section>
+
+        <!-- SNIPPET - PARTNERS -->
+        <?php snippet('partners') ?>
+    </div>
 
 <!-- SNIPPET FOOTER -->
 <?php snippet('footer') ?>
