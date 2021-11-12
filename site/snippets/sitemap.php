2833b0b96b2e4f7cf4cb3dc7d83c3feea2dc1ac8
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2021-11-12 16:23:23 +0100

    test

--- site/snippets/sitemap.php
+++ site/snippets/sitemap.php
@@ -6,10 +6,5 @@
     
     // Loop through links
     foreach ($sitemap as $link): ?>
-<<<<<<< HEAD
         <a class="footer__content__block__link" href="<?= $link->page()->toPage()->url() ?>"><?= $link->anchor() ?></a>
-=======
-        <a class="footer__content__block__link" href="<?=$link->pages()->url()?>"><?= $link->anchor() ?></a>
-        
->>>>>>> 4541d7b22a121db8aea5209f0bfc4f62af24bb0b
     <?php endforeach; ?>
