83f49081cb2be56a8564111f3ca0b658ec659b2a
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2021-11-13 18:26:57 +0100

    made more responsive + clean code

--- build/js/tabbar-accessibility.js
+++ build/js/tabbar-accessibility.js
@@ -1,26 +1,29 @@
-let tabHeader = document.getElementsByClassName("tab-header")[0];
-let tabIndicator = document.getElementsByClassName("tab-indicator")[0];
-let tabBody = document.getElementsByClassName("tab-body")[0];
+let tabHeader = document.querySelector(".tab-header");
+let tabIndicator = document.querySelector(".tab-indicator");
+let tabBody = document.querySelector(".tab-body");
 let tabsPane = tabHeader.getElementsByTagName("div");
+
+var activeClass = "active";
 
 
 
 //Add class active to first body of accessibility tab body
 var j = 0;
 var allDescriptions = tabBody.getElementsByTagName("div");
-allDescriptions[j].classList.add("active");
+allDescriptions[j].classList.add(activeClass);
 
 
 
 // To change active tab
 for(let i=0; i < tabsPane.length; i++) {
     tabsPane[i].addEventListener("click", function() {
-        tabHeader.getElementsByClassName("active")[0].classList.remove("active");
-        tabsPane[i].classList.add("active");
+        tabHeader.querySelector("." + activeClass).classList.remove(activeClass);
+        tabsPane[i].classList.add(activeClass);
 
-        tabBody.getElementsByClassName("active")[0].classList.remove("active");
-        tabBody.getElementsByTagName("div")[i].classList.add("active");
+        tabBody.querySelector("." + activeClass).classList.remove(activeClass);
+        tabBody.getElementsByTagName("div")[i].classList.add(activeClass);
 
+        // divide by how many tabs
         tabIndicator.style.left = `calc(calc(100% / 4) * ${i})`;
     });
 }
--- build/js/tabbar-manufacturers.js
+++ build/js/tabbar-manufacturers.js
@@ -1,34 +1,29 @@
-let tabHeaderManufacturer = document.getElementsByClassName("tab-header-manufacturer")[0];
-let tabIndicatorManufacturer = document.getElementsByClassName("tab-indicator-manufacturer")[0];
-let tabBodyManufacturer = document.getElementsByClassName("tab-body-manufacturer")[0];
+let tabHeaderManufacturer = document.querySelector(".tab-header-manufacturer");
+let tabIndicatorManufacturer = document.querySelector(".tab-indicator-manufacturer");
+let tabBodyManufacturer = document.querySelector(".tab-body-manufacturer");
 let tabsPaneManufacturer = tabHeaderManufacturer.getElementsByTagName("div");
+
+var activeClass = "active";
 
 
 
 //Add class active to first body of accessibility tab body
 var j = 0;
 var allDescriptionsManufacturer = tabBodyManufacturer.getElementsByTagName("div");
-allDescriptionsManufacturer[j].classList.add("active");
-
-//allDescriptionsManufacturer[j].classList.add("active-manufacturer");
+allDescriptionsManufacturer[j].classList.add(activeClass);
 
 
 
 // To change active tab
 for(let i=0; i < tabsPaneManufacturer.length; i++) {
     tabsPaneManufacturer[i].addEventListener("click", function() {
-        tabHeaderManufacturer.getElementsByClassName("active")[0].classList.remove("active");
-        tabsPaneManufacturer[i].classList.add("active");
+        tabHeaderManufacturer.querySelector("." + activeClass).classList.remove(activeClass);
+        tabsPaneManufacturer[i].classList.add(activeClass);
 
-        tabBodyManufacturer.getElementsByClassName("active")[0].classList.remove("active");
-        tabBodyManufacturer.getElementsByTagName("div")[i].classList.add("active");
+        tabBodyManufacturer.querySelector("." + activeClass).classList.remove(activeClass);
+        tabBodyManufacturer.getElementsByTagName("div")[i].classList.add(activeClass);
 
-        //tabHeaderManufacturer.getElementsByClassName("active-manufacturer")[0].classList.remove("active-manufacturer");
-        //tabsPaneManufacturer[i].classList.add("active-manufacturer");
-
-        //tabBodyManufacturer.getElementsByClassName("active-manufacturer")[0].classList.remove("active-manufacturer");
-        //tabBodyManufacturer.getElementsByTagName("div")[i].classList.add("active-manufacturer");
-
+        // divide by how many tabs
         tabIndicatorManufacturer.style.left = `calc(calc(100% / 2) * ${i})`;
     });
 }
