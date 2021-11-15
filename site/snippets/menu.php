8d65e81278ff4415ec97818cbecd95148bd6bed3
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2021-11-15 12:06:33 +0100

    fixed drop down nav on desktop

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -2,7 +2,7 @@
     <nav class="header__menu"> <!-- nav-scrolled -->
         <a class="header__logo logo" href="<?= $site->url() ?>"><img class="header__logo__img" src="assets/images/logo/fablab_logo.svg" alt="logo Fablab"></a>
 
-        <?php snippet('pages-list') ?>
+        <?php snippet('pages-list2') ?>
 
         <a class="header__languagebutton" href="#">
             <img class="header__languagebutton__flag" src="assets/images/be.svg">
