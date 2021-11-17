36b5d52aa4d220d8ea38288ee1f180d887540953
Cedric Lenders <cedric.lenders@student.kdg.be>
2021-11-17 15:03:37 +0100

    added newline css

--- site/templates/blog.php
+++ site/templates/blog.php
@@ -15,18 +15,7 @@
 
         <?php endforeach ?>
 
-<h1 class="h1"><?= $page->Subtitle()->html() ?></h1>
 
-<div class="content-blog__tag-filter">
-    <?php foreach($tags as $tag): ?>
-  
-       <a class="content-blog__tag-filter__button-tag" href="<?= url($page->url(), ['params' => ['tag' => $tag]]) ?>">
-        <?= html($tag) ?>
-      </a>
-
-    <?php endforeach ?>
-    
-</div>
 
 <div class="blog-wrapper">
     <?php foreach($articles as $article): ?>
