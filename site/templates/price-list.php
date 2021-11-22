1813560642be8b4a6a2c3750d4141cb787e37a4b
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2021-11-22 16:50:52 +0100

    changed scss

--- site/templates/price-list.php
+++ site/templates/price-list.php
@@ -5,27 +5,23 @@
     <?php snippet('menu-white') ?>
 
     <div class="price-list-container">
-    <h1 class="price-list-container__title h1"><?= $page->title() ?></h1>
-    <?php 
-        $standardmat = $page->standardmaterials();
-        $ownmat = $page->ownmaterials();
-        $materialstructure = $page->materials()->toStructure();
-    ?>
+        <h1 class="price-list-container__title h1"><?= $page->title() ?></h1>
+        <?php 
+            $standardmat = $page->standardmaterials();
+            $ownmat = $page->ownmaterials();
+            $materialstructure = $page->materials()->toStructure();
+        ?>
 
+        <p class="price-list-container__paragraph p">
+            <?= $standardmat?>
+            <br><br>
+        </p>
         
-            <p class="price-list-container__paragraph p">
-                <?= $standardmat?>
-                <br><br>
-
-            </p>
-        
-            <p class="price-list-container__paragraph p">
-                 <?= $ownmat?>
-            </p>
+        <p class="price-list-container__paragraph p">
+            <?= $ownmat?>
+        </p>
       
         <div class="price-list-container__material-container">
-        
-        
             <?php 
             $i = 0;
             foreach ($materialstructure as $item): 
@@ -46,39 +42,36 @@
                             <td class="price-list-container__material-container__table-container__table__table-row__table-dimension">Bedrijf</td>
                             <td class="price-list-container__material-container__table-container__table__table-row__table-dimension">In Stock</td>
                         </tr>
-                        <?php
-                            foreach($materialitem as $item):
-                        ?>
-                        <tr class="<?php 
-                            if($i%2 == 0){
-                                echo "oneven";
-                                $i++;
-                            }
-                            else{
-                                echo "even";
-                                $i++;
-                            }
-                        ?>">    
-                            <td><?= $item->detail() ?></td>
-                            <td><?= $item->color() ?></td>
-                            <td><?= $item->size() ?></td>
-                            <td>€<?= $item->pricekdg() ?></td>
-                            <td>€<?= $item->priceextern() ?></td>
-                            <td>€<?= $item->pricecompany() ?></td>
-                            <td> 
-                                <?php 
-                                    if($item->instock() == "true"){
-                                        echo('<i class="fa fa-check" aria-hidden="true"></i>');
-                                    }
-                                    else{
-                                        echo('<i class="fa fa-times" aria-hidden="true"></i>');
-                                    }
-                                ?> 
-                            </td> 
-                        </tr>
-                        <?php
-                            endforeach;
-                        ?>
+                        <?php foreach($materialitem as $item): ?>
+                            <tr class="<?php 
+                                if($i%2 == 0){
+                                    echo "oneven";
+                                    $i++;
+                                }
+                                else{
+                                    echo "even";
+                                    $i++;
+                                }
+                            ?>">  
+                              
+                                <td><?= $item->detail() ?></td>
+                                <td><?= $item->color() ?></td>
+                                <td><?= $item->size() ?></td>
+                                <td>€<?= $item->pricekdg() ?></td>
+                                <td>€<?= $item->priceextern() ?></td>
+                                <td>€<?= $item->pricecompany() ?></td>
+                                <td> 
+                                    <?php 
+                                        if($item->instock() == "true"){
+                                            echo('<i class="fa fa-check" aria-hidden="true"></i>');
+                                        }
+                                        else{
+                                            echo('<i class="fa fa-times" aria-hidden="true"></i>');
+                                        }
+                                    ?> 
+                                </td> 
+                            </tr>
+                        <?php endforeach; ?>
                         
                     </table>
                 </div>
