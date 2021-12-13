cccd689abba7360072226e4261692b9080d7912f
Cedric Lenders <cedric.lenders@student.kdg.be>
2021-12-13 15:13:38 +0100

    added id categories to json

--- site/templates/categories.json.php
+++ site/templates/categories.json.php
@@ -8,6 +8,7 @@ foreach($data as $category) {
 
   $json[] = [
     'name' => (string)$category->title(),
+    'id' => (string)$category->id(),
     'url'   => (string)$category->url(),
     'description' => (string)$category->info()
   ];
