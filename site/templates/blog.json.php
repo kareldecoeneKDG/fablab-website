32ca9423a8d5cc6b2eb4a4f00fe067d3ea34083f
Cedric Lenders <cedric.lenders@student.kdg.be>
2021-12-13 13:57:13 +0100

    begin machines to json

---
+++ site/templates/blog.json.php
@@ -0,0 +1,21 @@
+<?php
+
+$data = $pages->find('categories')->children()->children()->published();
+$json = [];
+$imageSource;
+foreach($data as $machine) {
+    
+    if ($machine->images()->isNotEmpty()){
+        $imageSource =  $machine->images()->first()->url();
+    }
+  $json[] = [
+    'name' => (string)$machine->title(),
+    'url'   => (string)$machine->url(),
+    'category' => (string)$machine->parent()->title(),
+    'image-source' => (string)$imageSource,
+    'available' => (string)$machine->isAvailable()
+  ];
+
+}
+
+echo json_encode($json);
