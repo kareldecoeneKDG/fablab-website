1c2f2f530f4e7577126a36d47d4f7e47a55aba42
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2022-01-10 15:28:34 +0100

    ux/ui comments almost all fixed

--- site/snippets/general/breadcrumb-content.php
+++ site/snippets/general/breadcrumb-content.php
@@ -1,12 +1,12 @@
 <?php foreach ($site->breadcrumb()->not('home') as $crumb) : ?>
     <?php if ($crumb->isActive()) : ?>
         <span typeof="v:Breadcrumb">
-            <span class="breadcrumb_last" property="v:title">
+            <span class="breadcrumb-item breadcrumb_last" property="v:title">
                 <?php echo $crumb->title() ?>
             </span>
         </span>
     <?php else : ?>
-        <span typeof="v:Breadcrumb">
+        <span class="breadcrumb-item" typeof="v:Breadcrumb">
             <a href="<?php echo $crumb->url() ?>" rel="v:url" property="v:title">
                 <?php echo $crumb->title() ?>
             </a>
