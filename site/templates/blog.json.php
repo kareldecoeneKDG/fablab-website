7c24fe54e49532531065bcaa70e0323b598a736a
Cedric Lenders <cedric.lenders@student.kdg.be>
2022-01-14 11:35:02 +0100

    added amount in Kirby

--- site/templates/blog.json.php
+++ site/templates/blog.json.php
@@ -9,14 +9,30 @@ foreach($data as $machine) {
     if ($machine->images()->isNotEmpty()){
         $imageSource =  $machine->images()->first()->url();
     }
-  $json[] = [
-    'name' => (string)$machine->title(),
-    'id' => (string)$machine->id(),
-    'url'   => (string)$machine->url(),
-    'category' => (string)$machine->parent()->title(),
-    'image-source' => (string)$imageSource,
-    'available' => (string)$machine->isAvailable()
-  ];
+    if((int)(string)$machine->amount()>1){
+      for ($i=1; $i <= (int)(string)$machine->amount(); $i++) { 
+        # code...
+        $json[] = [
+          'name' => (string)$machine->title() . ' ' .$i,
+          'id' => (string)$machine->id(),
+          'url'   => (string)$machine->url(),
+          'category' => (string)$machine->parent()->title(),
+          'image-source' => (string)$imageSource,
+          'available' => (string)$machine->isAvailable()
+        ];
+      }
+    }
+    else{
+      $json[] = [
+        'name' => (string)$machine->title(),
+        'id' => (string)$machine->id(),
+        'url'   => (string)$machine->url(),
+        'category' => (string)$machine->parent()->title(),
+        'image-source' => (string)$imageSource,
+        'available' => (string)$machine->isAvailable()
+      ];
+    }
+  
 
 }
 
